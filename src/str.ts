63aeb3d26ac1fa70f40e8944697ce055b9d85c44
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-04-25 20:38:21 +0530

    string helper files added

---
+++ src/str.ts
@@ -0,0 +1,106 @@
+import { StringObject } from "@laress/contracts";
+
+export class Str {
+
+    /**
+     * The cache of snake-cased words.
+     */
+    protected static snakeCache: StringObject = {};
+
+    /**
+     * The cache of camel-cased words.
+     */
+    protected static camelCache: StringObject = {};
+
+    /**
+     * The cache of studly-cased words.
+     */
+    protected static studlyCache: StringObject = {};
+
+    /**
+     * Convert a string to kebab case.
+     *
+     * @param value
+     */
+    public static kebab(value: string): string {
+        return Str.snake(value).replace(/_/ug, '-');
+    }
+
+    /**
+     * Convert a string to snake case.
+     *
+     * @param value
+     */
+    public static snake(value: string): string {
+
+        let key = value;
+
+        if (Str.snakeCache[value]) {
+            return Str.snakeCache[value];
+        }
+        // Remove all spaces after first letter of words
+        // are capitalized.
+        value = Str.ucwords(value).replace(/\s+/ug, '');
+        // Add an underscore before the capital letters.
+        // And convert the whole string to lower case.
+        value = value.replace(/(.)(?=[A-Z])/ug, '$1_').toLocaleLowerCase();
+
+        return Str.snakeCache[key] = value;
+    }
+
+    /**
+     * Convert a value to camel case.
+     *
+     * @param  value
+     */
+    public static camel(value: string): string {
+        if (Str.camelCache[value]) {
+            return Str.camelCache[value];
+        }
+        return Str.camelCache[value] = Str.lcfirst(Str.studly(value));
+    }
+
+    /**
+     * Convert a value to studly caps case.
+     *
+     * @param  value
+     */
+    public static studly(value: string): string {
+        let key = value;
+
+        if (Str.studlyCache[key]) {
+            return Str.studlyCache[key];
+        }
+        value = Str.ucwords(value.replace(/-|_/g, ' '));
+
+        return Str.studlyCache[key] = value.replace(/\ /g, '');
+    }
+
+    /**
+     * Convert the first character of the given string to lower case
+     * 
+     * @param str 
+     */
+    public static lcfirst(str: string) {
+        return str.charAt(0).toLocaleLowerCase() + str.slice(1);
+    }
+
+    /**
+     * Convert the first character of the given string to upper case
+     * 
+     * @param str 
+     */
+    public static ucfirst(str: string) {
+        return str.charAt(0).toLocaleUpperCase() + str.slice(1);
+    }
+
+    /**
+     * Upper case the first char of all the words in the string.
+     * 
+     * @param str 
+     */
+    public static ucwords(str: string) {
+        return str.trim().split(" ").map(word => Str.ucfirst(word)).join(" ");
+    }
+
+}
