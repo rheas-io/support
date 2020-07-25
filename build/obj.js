49fc83fd078902f13d0a9bdc62583d50ec7f40c6
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-07-25 19:36:38 +0530

    removed support for dotted keys

--- build/obj.js
+++ build/obj.js
@@ -28,42 +28,10 @@ class Obj {
         if (object.hasOwnProperty(key)) {
             return object[key];
         }
-        /*
-        const result = key.split('.').reduce((prev: any, current: any) => {
+        const result = key.split('.').reduce((prev, current) => {
             return (prev && prev[current] !== undefined) ? prev[current] : undefined;
         }, object);
-*/
-        const result = test(object, key);
         return result === undefined ? defaultValue : result;
     }
 }
 exports.Obj = Obj;
-/**
- * This could be used to iterate objects with dots on keys like
- *
- * {'a.b' : { 'c.d' : 100 } } ---->a.b.c.d will return 100
- *
- * But the issue arises when there are keys like
- *
- * {'a.b' : { 'c.d' : 100 }, 'a.b.c.d' : 2000 }
- * in the above case, there are two possible values for
- * a.b.c.d == 100 and 2000. Both are valid and there is no way to tell
- * which one is user referring to.
- *
- * So having no dots in the keys are the best option and dots are used
- * only for nested key access.
- *
- * @param dotted
- * @param mainKey
- */
-function test(dotted, mainKey) {
-    for (let key in dotted) {
-        if (mainKey === key) {
-            return dotted[key];
-        }
-        if (mainKey.startsWith(key + '.') && mainKey.length > key.length + 1) {
-            return test(dotted[key], mainKey.substring(key.length + 1));
-        }
-    }
-    return undefined;
-}
